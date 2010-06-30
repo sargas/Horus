d97b7288f38651ef7b30448d645079ff1a049e08
Joseph Jon Booker <joe@neoturbine.net>
2010-06-30 14:49:37 -0500

    Use forked expr library for parsing equations.
    Add eclipse's .classpath to repo
    Show signs of busyness in UI

--- src/net/neoturbine/horus/GraphData.java
+++ src/net/neoturbine/horus/GraphData.java
@@ -1,20 +1,26 @@
 package net.neoturbine.horus;
 
+import expr.Expr;
+
 public final class GraphData {
 	private final float[] _data;
 	private final float _minX, _minY, _maxX, _maxY;
+	private final Expr _expr;
 	public GraphData(float[] data, 
 			float minX, float minY,
-			float maxX, float maxY) {
+			float maxX, float maxY,
+			Expr expr) {
 		_data = data;
 		_minX = minX; _maxX = maxX;
 		_minY = minY; _maxY = maxY;
+		_expr = expr;
 	}
 	public float[] getData() {return _data;}
 	public float getMinX() {return _minX;}
 	public float getMinY() {return _minY;}
 	public float getMaxX() {return _maxX;}
 	public float getMaxY() {return _maxY;}
+	public Expr getExpression() {return _expr; }
 	public String toString() {
 		StringBuilder out = new StringBuilder();
 		out .append("Independent Axis (")
--- src/net/neoturbine/horus/OrbitDiagram.java
+++ src/net/neoturbine/horus/OrbitDiagram.java
@@ -1,9 +1,16 @@
 package net.neoturbine.horus;
 
+import expr.Expr;
+import expr.Parser;
+import expr.SyntaxException;
+import expr.Variable;
+import expr.VariableFactory;
 import net.neoturbine.horus.view.GraphView;
 import android.app.Activity;
 import android.os.AsyncTask;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.Window;
 
 /**
  * Activity for viewing orbit diagrams as described in TODO
@@ -26,15 +33,18 @@ public class OrbitDiagram extends Activity {
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
+		requestWindowFeature(Window.FEATURE_INDETERMINATE_PROGRESS);
 		setContentView(R.layout.main);
-
+		
 		orbit = (GraphView) findViewById(R.id.orbit);
 
 		final Object data = getLastNonConfigurationInstance();
 
-		if(data == null)
+		if(data == null) {
+			setTitle(R.string.working);
+			setProgressBarIndeterminateVisibility(true);
 			(new calcTask()).execute(0,1);
-		else
+		} else
 			setData((GraphData)data);
 	}
 
@@ -60,6 +70,8 @@ public class OrbitDiagram extends Activity {
 				result.getMinY(),
 				result.getMaxX(),
 				result.getMaxY());
+		//setTitle(result.getExpression().toString());
+		setTitle("Calculated"); //TODO: replace by map string
 	}
 
 	/**
@@ -79,25 +91,43 @@ public class OrbitDiagram extends Activity {
 
 			int j = 0;
 			float[] data = new float[resA*perit*2];
-			float minScatter = Float.MAX_VALUE;
-			float maxScatter = Float.MIN_VALUE;
+			double minScatter = Float.MAX_VALUE;
+			double maxScatter = Float.MIN_VALUE;
+			
+			Expr expr;
+			Variable var_x = VariableFactory.make("x");
+			Variable var_A = VariableFactory.make("A");
+			Parser parser = new Parser();
+			parser.allow(var_x);parser.allow(var_A);
+			try {
+				expr = parser.parseString("A*(x-x^3)");
+			} catch (SyntaxException e) {
+				Log.v("Horus",e.explain());
+				return null;
+			}
 
-			for(int z=0;z<resA;z++) {
-				double A = 1.5 + (2.59807621135-1.5)/resA * z;
-				double x = Math.random();
-				for(int k=1;k<200;++k) {
-					x = (A*x*(1-Math.pow(x,2)));
+			float A = 1.5f;
+			final float diffA = (2.59807621135f-1.5f)/resA;
+			for(int z=0; z<resA; z++,A += diffA) {
+				var_A.setValue(A);
+				var_x.setValue(Math.random());
+				for(int k=0;k<200;++k) {
+					var_x.setValue(expr.value());
 				}
-				for(int i=j; i<j+perit-1;++i) {
-					x = A*x*(1-Math.pow(x,2));
-					if(x < minScatter) minScatter = (float)x;
-					else if(x > maxScatter) maxScatter = (float)x;
-					data[2*i] = (float)A;
-					data[2*i+1] = (float)x;
+				for(int i=2*j; i<2*(j+perit-1);i+=2) {
+					var_x.setValue(expr.value());
+					final double xval = var_x.value();
+					if(xval < minScatter)
+						minScatter = xval;
+					else if(xval > maxScatter)
+						maxScatter = xval;
+					data[i] = A;
+					data[i+1] = (float)xval;
 				}
 				j=j+perit;
 			}
-			return new GraphData(data,1.5f,minScatter,2.59807621135f,maxScatter);
+			return new GraphData(data,1.5f,(float)minScatter,
+					2.59807621135f,(float)maxScatter,expr);
 		}
 
 		/**
@@ -106,6 +136,7 @@ public class OrbitDiagram extends Activity {
 		 */
 		protected void onPostExecute(GraphData result) {
 			OrbitDiagram.this.setData(result);
+			OrbitDiagram.this.setProgressBarIndeterminateVisibility(false);
 		}
 	}
 }
